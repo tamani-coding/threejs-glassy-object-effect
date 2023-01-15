3638cfa15e15b4eaa8aa07f1f7382f8839228f1a
genka <tamani.coding@gmail.com>
2023-01-15 12:48:36 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -51,8 +51,13 @@ controls.update();
 
 // GUI
 const gui = new GUI();
-const materialParams = gui.addFolder('physical-material-params');
-materialParams.open();
+const glassParams = gui.addFolder('glass-material-params');
+const metalParams = gui.addFolder('metal-material-params');
+glassParams.open();
+metalParams.open();
+
+let glassObject;
+let metalObject;
 
 const envTexture = new THREE.CubeTextureLoader().load([
     'assets/pos-x.png',
@@ -68,47 +73,70 @@ scene.background = envTexture
 
 const normalMap = new THREE.TextureLoader().load('assets/Abstract_011_normal.jpg');
 
-// for glass objects - ior (reflectivity), thickness, transmission > 0, clearcoat, roughness
-const glassMaterial = new THREE.MeshPhysicalMaterial({
-    normalMap: normalMap,
-    // color: 0x72147E,
-    color: 0xFFFFFF,
-} as THREE.MeshPhysicalMaterialParameters);
-// TODO sheen, roughnessMap, transmissionMap, attenuationTint, normalMap, environmentMap
-glassMaterial.clearcoat = 0.1;
-glassMaterial.ior = 1.5;
-// physicalMaterial.reflectivity = 0.5;
-glassMaterial.specularIntensity = 0.1;
-glassMaterial.roughness = 0.1;
-glassMaterial.thickness = 1;
-glassMaterial.transmission = 0.99;
-glassMaterial.metalness = 0.0;
-glassMaterial.sheen = 0.7;
-glassMaterial.sheenColor = new THREE.Color(0xFFFFFF);
+function initGlassObject() {
+    // for glass objects - ior (reflectivity), thickness, transmission > 0, clearcoat, roughness
+    const glassMaterial = new THREE.MeshPhysicalMaterial({
+        normalMap: normalMap,
+        // color: 0x72147E,
+        color: 0xFFFFFF,
+    } as THREE.MeshPhysicalMaterialParameters);
+    // TODO sheen, roughnessMap, transmissionMap, attenuationTint, normalMap, environmentMap
+    glassMaterial.clearcoat = 0.1;
+    glassMaterial.ior = 1.5;
+    glassMaterial.specularIntensity = 0.1;
+    glassMaterial.roughness = 0.1;
+    glassMaterial.thickness = 1;
+    glassMaterial.transmission = 0.99;
+    glassMaterial.sheen = 0.9;
+    glassMaterial.sheenColor = new THREE.Color(0xFFFFFF);
 
+    glassParams.add(glassMaterial, 'clearcoat').min(0).max(1);
+    glassParams.add(glassMaterial, 'ior').min(1).max(2.33);
+    glassParams.add(glassMaterial, 'specularIntensity').min(0).max(1);
+    glassParams.add(glassMaterial, 'roughness').min(0).max(1);
+    glassParams.add(glassMaterial, 'thickness').min(0).max(10);
+    glassParams.add(glassMaterial, 'transmission').min(0).max(1);
+    glassParams.add(glassMaterial, 'sheen').min(0).max(1);
 
-materialParams.add(glassMaterial, 'clearcoat').min(0).max(1);
-materialParams.add(glassMaterial, 'ior').min(1).max(2.33);
-// materialParams.add(physicalMaterial, 'reflectivity').min(0).max(1);
-materialParams.add(glassMaterial, 'specularIntensity').min(0).max(1);
-materialParams.add(glassMaterial, 'roughness').min(0).max(1);
-materialParams.add(glassMaterial, 'thickness').min(0).max(10);
-materialParams.add(glassMaterial, 'transmission').min(0).max(1);
-materialParams.add(glassMaterial, 'metalness').min(0).max(1);
-materialParams.add(glassMaterial, 'sheen').min(0).max(1);
+    // IcosahedronGeometry
+    glassObject = new THREE.Mesh(new THREE.SphereGeometry(1, 64, 64), glassMaterial);
+    glassObject.position.z = 2;
+    glassObject.position.x = 2;
+    glassObject.position.y = 1.7;
+    glassObject.castShadow = true;
+    scene.add(glassObject);
+}
 
-// IcosahedronGeometry
-const glassObject = new THREE.Mesh(new THREE.SphereGeometry(1, 64, 64), glassMaterial);
-glassObject.position.z = 2;
-glassObject.position.x = 2;
-glassObject.position.y = 1.5;
-glassObject.castShadow = true;
-scene.add(glassObject);
+function initMetalObject() {
+    // for metal objects - transmission = 0, metalness, clearcoat, roughness
+    const metalMaterial = new THREE.MeshPhysicalMaterial({
+        normalMap: normalMap,
+        color: 0xFFFFFF,
+    } as THREE.MeshPhysicalMaterialParameters);
+    metalMaterial.clearcoat = 0.1;
+    metalMaterial.roughness = 0.1;
+    metalMaterial.metalness = 1.0;
+    metalMaterial.sheen = 0.7;
+    metalMaterial.sheenColor = new THREE.Color(0x1363DF);
 
-// for metal objects - transmission = 0, metalness, clearcoat, roughness
+    metalParams.add(metalMaterial, 'clearcoat').min(0).max(1);
+    metalParams.add(metalMaterial, 'roughness').min(0).max(1);
+    metalParams.add(metalMaterial, 'metalness').min(0).max(1);
+    metalParams.add(metalMaterial, 'sheen').min(0).max(1);
 
+    // IcosahedronGeometry
+    metalObject = new THREE.Mesh(new THREE.SphereGeometry(1, 64, 64), metalMaterial);
+    metalObject.position.z = 3;
+    metalObject.position.x = 0;
+    metalObject.position.y = 1.7;
+    metalObject.castShadow = true;
+    scene.add(metalObject);
+}
 
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.25), new THREE.MeshPhongMaterial( { color : 0xFF7B54 } ));
+initGlassObject();
+// initMetalObject();
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.25), new THREE.MeshPhongMaterial({ color: 0xFF7B54 }));
 sphere.position.z = 0;
 sphere.position.x = 0;
 sphere.position.y = 1;
@@ -116,7 +144,7 @@ sphere.castShadow = true;
 scene.add(sphere);
 
 
-const ground = new THREE.Mesh(new THREE.BoxGeometry(15,15), new THREE.MeshPhongMaterial( { color: 0xF54748 } ));
+const ground = new THREE.Mesh(new THREE.BoxGeometry(15, 15), new THREE.MeshPhongMaterial({ color: 0xF54748 }));
 ground.rotation.x = - Math.PI / 2;
 ground.rotation.x = - Math.PI / 2;
 ground.receiveShadow = true;
@@ -137,10 +165,18 @@ function animate() {
     TWEEN.update();
 
     const delta = clock.getDelta();
-    glassObject.rotation.x += delta * 0.2;
-    glassObject.rotation.z += delta * 0.2;
 
-    // object.position.y = 1.5 + Math.sin(Date.now() * 0.005) * 0.15
+    if (glassObject) {
+        glassObject.rotation.x += delta * 0.2;
+        glassObject.rotation.z += delta * 0.2;
+        glassObject.position.y = 1.7 + Math.sin(Date.now() * 0.005) * 0.10
+    }
+    if (metalObject) {
+        metalObject.rotation.x -= delta * 0.2;
+        metalObject.rotation.z -= delta * 0.2;
+        metalObject.position.y = 1.7 + Math.cos(Date.now() * 0.005) * 0.10
+    }
+
 
     requestAnimationFrame(animate);
 
